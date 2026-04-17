220dde9f63d629366a381bfd756d8bf12c1d34d6
javiercavalero <javicavalero56@gmail.com>
2026-04-17 00:23:16 -0300

    agrega home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../styles/home.css'
+import bg from '../assets/home.jpg' // poné tu imagen acá
 
 function Home() {
   return (
-    <div>Home</div>
+    <section className="home">
+      <img src={bg} alt="home" className="home-bg" />
+
+      <div className="home-overlay" />
+
+      <div className="home-content">
+        <h1>
+          ESTUDIO DE <br />
+          <span>ABOGADOS</span>
+        </h1>
+
+        <p>
+          Asesoramiento Jurídico Integral. <br />
+          Especialistas en Derecho Penal.
+        </p>
+      </div>
+    </section>
   )
 }
 
